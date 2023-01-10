7b56ee96ade2c71e4636e84a7f7b6efdff3d29c3
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 14:25:35 +0530

    Added : 2D Array

--- src/com/bridgelabz/functionalproblems/Array2D.java
+++ src/com/bridgelabz/functionalproblems/Array2D.java
@@ -1,2 +1,22 @@
-package com.bridgelabz.functionalproblems;public class Array2D {
+package com.bridgelabz.functionalproblems;
+
+import java.util.Scanner;
+public class Array2D {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Column = scanner.nextInt();
+        int Row = scanner.nextInt();
+        int[][] Array = new int[Row][Column];
+        for (int i = 0; i < Row; i++) {
+            for (int j = 0; j < Column; j++) {
+                Array[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < Row; i++) {
+            for (int j = 0; j < Column; j++) {
+                System.out.println(Array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
